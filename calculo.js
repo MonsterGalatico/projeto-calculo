92c609412b70783482c2691032db199480213183
MonsterGalatico <132069198+MonsterGalatico@users.noreply.github.com>
2023-09-28 12:05:14 -0300

    Add files via upload

---
+++ calculo.js
@@ -0,0 +1,13 @@
+//console.log("está funcionando");
+/*let anoDeNascimento = window.prompt("Em que ano você nasceu?");
+
+let anoDeNascimento = window.prompt("Em que ano vocẽ nasceu?");
+console.log(anoDeNascimento);
+alert("Quem nasceu " + anoDeNascimento + " vai fazer XXXXX anos em 2023");
+*/
+
+//----------------------------------------------------------------------------//
+//calculadora de nota usando a função prompt
+
+//aqui trás as notas do html e tira o conteúdo 
+let notaPrimTrim 
